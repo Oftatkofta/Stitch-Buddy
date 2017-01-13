233461768b3a256f658a08b2c5535563af6e1858
Oftatkofta <oftakofta@gotmail.com>
2017-01-13 15:48:05 +0100

    some PIV progress

--- PIV_buddy.py
+++ PIV_buddy.py
@@ -1,151 +1,235 @@
 from __future__ import print_function
-from stitch_buddy import *
-import openpiv.tools
+#import openpiv.tools
 import openpiv.process
+import openpiv.filters
 import openpiv.scaling
+import openpiv.validation
+import pylab as plt
+import numpy as np
+import math
+from PIL import Image
+from matplotlib.colors import LogNorm
+
+try:
+    import tiffile as tiffile
+except:
+    pass
+
+indir = "/Volumes/HDD/Python_laboratory/Vector_visualizer/test"
+outdir = "/Volumes/HDD/Python_laboratory/Vector_visualizer/test/out/"
+testfile = "/Volumes/HDD/Python_laboratory/Vector_visualizer/test/sub.tif"
+
+def display(array):
+    plt.imshow(array, cmap="viridis")
+    plt.show()
+
+def polar_bar(theta, radii, color, saveFlag, fname="tst"):
+
+
+    ax = plt.subplot(111, projection='polar')
+    img = plt.scatter(theta, radii, c=color)
+    img.set_cmap('viridis')
+
+    #plt.show()
+    if saveFlag:
+        plt.savefig(outdir+fname+".png", bbox_inches='tight', pad_inches = 0)
+        plt.close()
+        im = Image.open(outdir+fname+".png")
+        #im = im.convert("L")
+        im.save(outdir+fname+".jpg")
+        im.close()
+
+    if not saveFlag:
+        plt.show()
+
+
+def saveQiver(x,y,u,v, fname, l=None):
+    img = plt.quiver(x, y, u, v, l)
+    #img.set_cmap('viridis')
+    #plt.axis('off')
+    #plt.show()
+    plt.savefig(outdir+fname+".png", bbox_inches='tight', pad_inches = 0)
+    plt.close()
+    im = Image.open(outdir+fname+".png")
+    im.save(outdir+fname+".jpg")
+    im.close()
+
+
+def saveScatter(arrX, arrY, fname):
+    filename =  outdir + fname + ".png"
+    img = plt.scatter(arrX, arrY, marker=".", c="black")
+    #img.set_cmap('viridis')
+    plt.axis('off')
+    # plt.show()
+    plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+    plt.close()
+    im = Image.open(filename)
+    im = im.convert("L")
+    im = im.resize((400,400))
+    im.save(filename)
+    im.close()
+
+def save2Dhist(x, y, bins, fname):
+    plt.hist2d(x, y, bins=bins, norm=LogNorm())
+    plt.colorbar()
+    plt.set_cmap('viridis')
+    plt.savefig(outdir + fname + ".png")
+    plt.close()
+
+def arrayProcessor(arr):
+
+    #PIV parameters
+    window_size = 8
+    overlap = 0
+    search_area = 10
+
+
+    x, y = openpiv.process.get_coordinates(image_size=arr[0].shape, window_size=window_size, overlap=overlap)
+
+    for frame in range(arr.shape[0]):
+        if frame == arr.shape[0]-1:
+            break
+
+        frame_a = arr[frame]
+        frame_b = arr[frame+1]
+
+
+        u, v, sig2noise = openpiv.process.extended_search_area_piv( frame_a, frame_b, window_size=window_size,
+                                                                overlap=overlap,
+                                                                dt=1, search_area_size=search_area,
+                                                                sig2noise_method='peak2peak' )
+
+        u, v, mask = openpiv.validation.sig2noise_val( u, v, sig2noise, threshold = 1.5)
+        u, v = openpiv.filters.replace_outliers(u, v, method='localmean', kernel_size=2)
+
+        size_x = u.shape[-2]
+        size_y = u.shape[-1]
+        n_pixels = size_x * size_y
+
+        # u, v, mask = openpiv.validation.sig2noise_val(u, v, sig2noise, threshold=noise_threshold)
+        vector_lengths = np.sqrt((u ** 2 + v ** 2))
+        dot_products = np.zeros(u.shape)
+        vector_angles = np.zeros(u.shape)
+        dist_out = []
+        angles_out = []
+        for i in range(n_pixels):  # outer loop for vector a
+            a_m = i % size_x
+            a_n = i // size_y
+            vector_a = np.array((u[a_m][a_n], v[a_m][a_n]))
+            distances_to_a = np.sqrt((x - x[a_m][a_n]) ** 2 + (
+            y - y[a_m][a_n]) ** 2)  # Maxtrx where each value is its pixel distance to a in the original image
+            ab_length_products = vector_lengths[a_m][a_n] * vector_lengths
+            for j in range(n_pixels):  # inner loop for vector b
+                if j >= i:
+                    break  # no need to double count pairs or calculate self-angles
+
+                b_m = j % size_x
+                b_n = j // size_y
+                vector_b = np.array((u[b_m][b_n], v[b_m][b_n]))
+
+                dot_products[b_m][b_n] = np.dot(vector_a, vector_b)
+
+            vector_angles = np.arccos(dot_products / ab_length_products)
+            dist_out.extend(distances_to_a.flatten().tolist())
+            angles_out.extend(vector_angles.flatten().tolist())
+
+        save2Dhist(dist_out, angles_out, 30, str(frame))
+
+
+with tiffile.TiffFile(testfile) as tif:
+    arr = tif.asarray()
+    arr = arr.astype(np.int32)
+    print(arr.shape)
+
+arrayProcessor(arr)
+
+# window_size = 8
+# overlap = 0
+# search_area = 10
+# noise_threshold = 1.5
+#
+#
+# x, y = openpiv.process.get_coordinates(image_size=arr[0].shape, window_size=window_size, overlap=overlap)
+#
+# frame_a = arr[0]
+# frame_b = arr[1]
+#
+# u, v, sig2noise = openpiv.process.extended_search_area_piv(frame_a, frame_b, window_size=window_size, overlap=overlap,
+#                                                            dt=60 * 8, search_area_size=search_area,
+#                                                            sig2noise_method='peak2peak')
+#
+# size_x = u.shape[-2]
+# size_y = u.shape[-1]
+# n_pixels = size_x * size_y
+#
+# #u, v, mask = openpiv.validation.sig2noise_val(u, v, sig2noise, threshold=noise_threshold)
+# vector_lengths = np.sqrt((u ** 2 + v ** 2))
+# dot_products = np.zeros(u.shape)
+# vector_angles = np.zeros(u.shape)
+# dist_out = []
+# angles_out = []
+# for i in range(n_pixels): #outer loop for vector a
+#     a_m = i % size_x
+#     a_n = i // size_y
+#     vector_a = np.array( (u[a_m][a_n],v[a_m][a_n]) )
+#     distances_to_a = np.sqrt((x - x[a_m][a_n]) ** 2 + (y - y[a_m][a_n]) ** 2) #Maxtrx where each value is its pixel distance to a in the original image
+#     ab_length_products = vector_lengths[a_m][a_n] * vector_lengths
+#     for j in range(n_pixels): #inner loop for vector b
+#         if j >= i:
+#             break #no need to double count pairs or calculate self-angles
+#
+#         b_m = j % size_x
+#         b_n = j // size_y
+#         vector_b = np.array( (u[b_m][b_n],v[b_m][b_n]) )
+#
+#         dot_products[b_m][b_n] = np.dot(vector_a, vector_b)
+#
+#     vector_angles = np.arccos(dot_products / ab_length_products)
+#     dist_out.extend(distances_to_a.flatten().tolist())
+#     angles_out.extend(vector_angles.flatten().tolist())
+#
+# plt.hist2d(dist_out, angles_out, bins=40)
+# plt.show()
+
+        #polar_bar(vector_angles, vector_lengths, vector_dot_products, False )
+
+#display(vector_angles)
+#display(distances)
+#plt.scatter(distances, vector_angles)
+#plt.show()
+#x1, y1 = openpiv.process.get_coordinates(image_size=(20,20), window_size=5, overlap=0)
+
+#display(sig2noise)
 
 
 
-def filenamesToDict(indir, wellNameDict=None):
-
-    """
-    Transforms the .tif-files in a directory into a dictionary.
-
-    Filenames must conform to the following general pattern:
-    ..._wellID-xxx_threeDigitRowNumber_threeDigitColNumber...
-
-    :param
-    indir: path to directory with files from a multiwell mosaic experiment
-    wellNameDict: Dictionary of names to give the wells from the wellID-number
-
-    property_dict = 'nrows': (int) No. of rows in well
-                    'ncols': (int) No. of columns in well
-                    'nChans': (int) No. of channels in image
-                    'xpix': (int) No. pixels in X-dimension of images
-                    'ypix': (int) No. pixels in Y-dimension of images
-                    'frame_interval': (float) No. of time units between frames
-                    'timeUnit': (str) time unit
-                    'pixres': (float) size of pixels in resolution units
-                    'resUnit': (str) spatial resolution unit
-                    'pixelType':(str) pixeldepth of image
-                    'positions': position_dict gives file at position (row,col)
-                    'files':(list) names of files in well
-                    'isConcat':(bool) If the sequence is split in to multiple files
-                    'OME-XML':None <- Not implemented yet
-
-    position_dict = {(row, col):filename(s)}
+#u, v, mask = openpiv.validation.local_median_val(u, v, 1, 1)
 
 
+#plt.hist(l, 10, (0,0.01))
+#plt.scatter(x,y,c=l)
 
-    :return: Dictionary with wellID:property_dict
-    """
-    # Ingore non-.tif files in indir
-    filenames = [fname for fname in os.listdir(indir) if ".tif" in fname]
+#plt.show()
 
-    wellDict = {}
+#u, v = openpiv.filters.replace_outliers( u, v, method='localmean', kernel_size=2)
 
-    isConcat = False
+#x, y, u, v = openpiv.scaling.uniform(x, y, u, v, scaling_factor = 96.52 )
 
-    #Regex used to flexibly identify wells, rows, columns, and split files from filenames
+#
+#print(vectors[0,2])
+#print(vectors.shape)
+#
+# plt.scatter(u, v)
+#plt.show()
 
-    #Matches any number of digits preceded by "MMStack_" and followed by "-"
-    well_regex = re.compile("(?<=MMStack_)\d+(?=-)")
-
-    #Matches three digits preceded by three digits and "_"
-    column_regex = re.compile("(?<=\d{3}_)\d{3}")
-
-    #Matches three digits preceded by three digits and "_"
-    row_regex = re.compile("(?<=_)\d{3}(?=_)")
-
-    #Matches a digit preceded by three digits and "_", followed by ".ome"
-    concat_regex = re.compile("(?<=\d{3}_\d{3}_)\d(?=\.ome)")
-
-    first_file = os.path.join(indir, filenames[0])
-
-    with tiffile.TiffFile(first_file) as tif:
-        meta_data = tif.micromanager_metadata
-        frames = len(tif.pages)
-        page=tif[0]
-        pixres=page.tags['x_resolution'].value #Assumes equal x/y resolution
-        resoloution_unit = page.tags['resolution_unit'].value
-        pixelDepth = page.tags['bits_per_sample'].value
-        omexml = page.tags['image_description'].value
+##dot = np.dot(x,y)
+#>>> x_modulus = np.sqrt((x*x).sum())
+#>>> y_modulus = np.sqrt((y*y).sum())
+#>>> cos_angle = dot / x_modulus / y_modulus # cosine of angle between x and y
+#>>> angle = np.arccos(cos_angle)
+#>>> angle
+#0.80823378901082499
+#>>> angle * 360 / 2 / np.pi # angle in degrees
+#46.308384970187326
 
 
-
-    frame_interval = meta_data['summary']['WaitInterval']
-    ypix, xpix = meta_data['summary']['Height'], meta_data['summary']['Width']
-    nChannels =  meta_data['summary']['Channels']
-    nSlices =  meta_data['summary']['Slices']
-    nTimepoints = frames/(nChannels*nSlices)
-    resoloution_unit =  {1: 'none', 2: 'inch', 3: 'centimeter'}[resoloution_unit]
-
-    for f in filenames:
-        #Extract positioning information from filename with regex
-        wellID = int(well_regex.search(f).group())
-
-
-        if wellNameDict != None:
-            wellID = wellNameDict[wellID]
-
-
-        rowNumber = int(row_regex.search(f).group())
-        columnNumber = int(column_regex.search(f).group())
-        concat = concat_regex.search(f)
-        if concat != None:
-            isConcat = True
-
-        #If there is no key for wellID in wellDict -> create a dict of properties
-        if wellDict.get(wellID) == None:
-            wellDict[wellID] = {'nrows':1,
-                                'ncols':1,
-                                'nChannels':int(nChannels),
-                                'nSlices': int(nSlices),
-                                'xpix':int(xpix),
-                                'ypix':int(ypix),
-                                'nTimepoints':int(nTimepoints),
-                                'frame_interval':frame_interval,
-                                'timeunit':'ms',
-                                'pixel_resolution':pixres, #resolution stored as rational in tif tag
-                                'resoloution_unit': resoloution_unit,
-                                'pixelDepth':pixelDepth,
-                                'positions':{},
-                                'files':[],
-                                'isConcat':isConcat,
-                                'OME-XML':omexml
-                                }
-
-        #Populate Properties
-        wellDict[wellID]['nrows'] = max(rowNumber+1, wellDict[wellID]['nrows'])
-        wellDict[wellID]['ncols'] = max(columnNumber+1, wellDict[wellID]['ncols'])
-
-        #List of filenames for the well
-        wellDict[wellID]['files'].append(f)
-
-        #Dict with (row, column):(list) filename(s)
-        if wellDict[wellID]['positions'].get((rowNumber, columnNumber)) == None:
-            wellDict[wellID]['positions'][(rowNumber, columnNumber)]=[f]
-        else:
-            wellDict[wellID]['positions'][(rowNumber, columnNumber)].append(f)
-            wellDict[wellID]['positions'][(rowNumber, columnNumber)].sort()
-            wellDict[wellID]['isConcat'] = isConcat
-
-    return wellDict
-
-
-
-
-frame_a  = openpiv.tools.imread( 'exp1_001_a.bmp' )
-frame_b  = openpiv.tools.imread( 'exp1_001_b.bmp' )
-
-u, v, sig2noise = openpiv.process.extended_search_area_piv( frame_a, frame_b, window_size=24, overlap=12, dt=0.02, search_area_size=64, sig2noise_method='peak2peak' )
-
-x, y = openpiv.process.get_coordinates( image_size=frame_a.shape, window_size=24, overlap=12 )
-
-u, v, mask = openpiv.validation.sig2noise_val( u, v, sig2noise, threshold = 1.3 )
-
-u, v = openpiv.filters.replace_outliers( u, v, method='localmean', n_iter=10, kernel_size=2)
-
-x, y, u, v = openpiv.scaling.uniform(x, y, u, v, scaling_factor = 96.52 )
-
-openpiv.tools.save(x, y, u, v, 'exp1_001.txt' )
